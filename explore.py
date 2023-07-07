1b087a03ecd7d3ac3c4e7462933faa9a58648a8e
Juanc <jkaz.os@gmail.com>
2023-07-07 16:35:19 -0500

    Restructure analyzers as plotter decorators

--- explore.py
+++ explore.py
@@ -7,13 +7,13 @@ JCA
 import os
 import argparse
 from pathlib import Path
-
+from time import time
 
 import matplotlib.pyplot as plt
+from tqdm import tqdm
+import cv2
 
-
-from DatasetExplorer.analyzers import plot_mosaic, class_distribution, \
-    sizes, complete_color_hist
+from DatasetExplorer.analyzers import func_plotters
 from DatasetExplorer.auxfunc import create_out_dir
 
 parser = argparse.ArgumentParser(
@@ -21,38 +21,56 @@ parser = argparse.ArgumentParser(
                     description='Classification image dataset exploration')
 
 parser.add_argument('path', help='Dataset location')
+parser.add_argument('-c', '--cache', help='Load in memory dataset. This could use all the machine RAM', action='store_true')
 
 
-def main(path):
+def main(path, cache=False, show=False):
+    """ Analyze dataset located in path. Path is the direction to the root of 
+    the dataset containing the folder by class.
+    Args
+    :param path: (str) path to root direcotry of the dataset
+    :param cache: (bool) if true images of the dataset will be stored in memory
+    :param show: (bool)show plots while performing analysis  
+    """
+
     print(f' -- DATASET EXPLORER --')
-
     # Create output folder. All the plots will be saved here
     out_path = create_out_dir(path, tag='exp')
 
-
     classes = os.listdir(path)
-    print(f' - {len(classes)} in Dataset: {", ".join(classes)}')
+    print(f' - {len(classes)} classes')
     
     # Get all the files of the dataset
     files = list(Path(path).glob('**/*'))
-    print(f' - {len(files)} Images on dataset')
+    print(f' - {len(files)} images')
 
-    # Plot Mosaic
-    # plot_mosaic(files)
-    # plt.show()
+    dataset = {
+        'files': files,
+        # Some analyzers load all the images. Keep them for speed analysis.
+        # take into accound that this could use all the available RAM
+        'images': {},
+        'classes': classes,
+    }
 
-    # Class distribution
-    # class_distribution(files, classes)
-    # plt.show()
 
-    # Image sizez
-    # sizes(files)
-    # plt.show()
+    if cache:
+        print(f' - Loading images in memory...')
+        err = []
+        for im_path in tqdm(files, total=len(files)):
+            try:
+                im = cv2.imread(str(im_path))
+            except Exception as e:
+                err.append(im_path)
+            else:
+                 dataset['images'][im_path] = im
 
-    # Image classes color histogram
-    complete_color_hist(files, classes)
-    # plt.show()
-    plt.savefig("/Users/juanc/Downloads/archive/image_class_histogram.png")
+
+    init = time()
+
+    print(' - Running: ')
+    for func in func_plotters:
+        func(dataset, show=show, save_path=out_path)
+    print(f' -- Total time: {round(time()-init)} s --')
 
 
 
@@ -61,4 +79,4 @@ def main(path):
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    main(args.path)
+    main(args.path, cache=args.cache)
